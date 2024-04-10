da92ab557683725777aaab1faaf383580059f16f
Gabriel Piva <89921667+gabriel-piva@users.noreply.github.com>
2024-04-10 16:27:05 -0300

    correcao no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,14 +8,16 @@ function Header() {
 				<h1 className={styles.logo}>
 					<Link to="/">Step 3</Link>
 				</h1>
-				<ul className={styles.links}>
-					<li>
-						<Link to="/projects">Projects</Link>
-					</li>
-					<li>
-						<Link to="/login">Login</Link>
-					</li>
-				</ul>
+				<nav className={styles.links}>
+					<ul>
+						<li>
+							<Link to="/projects">Projects</Link>
+						</li>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+					</ul>
+				</nav>
 			</div>
 		</header>
 	);
